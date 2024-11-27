54fb0c6c5c0cd03068cd917aab2aafab4cf18774
hbandaru2 <hbandaru2@miraclesoft.com>
2024-11-27 20:25:46 +0530

    second commit

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -1,8 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(res: NextResponse) {
-  return NextResponse.json(
-    { message: "Check route is working." },
-    { status: 200 }
-  );
+export async function GET(request: NextRequest) {
+  // Example logic for the GET request
+  const response = { message: "Success" };
+
+  // Make sure to return the NextResponse object correctly
+  return NextResponse.json(response);
 }
