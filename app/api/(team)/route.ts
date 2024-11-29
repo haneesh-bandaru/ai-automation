ea9901ffdcb0057f62bba111067390f9bc57aaed
haneesh-bandaru <haneeshbandaru05@gmail.com>
2024-11-30 00:16:39 +0530

    fixed the issues

--- app/api/(team)/route.ts
+++ app/api/(team)/route.ts
@@ -3,4 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, res: NextResponse) {
 //   const projects = await Project.find();
 //   res.json(projects);
+return NextResponse.json({ message: "Hello from the users route!" });
+
 }
--- app/api/(users)/route.ts
+++ app/api/(users)/route.ts
@@ -3,4 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, res: NextResponse) {
 //   const projects = await Project.find();
 //   res.json(projects);
+return NextResponse.json({ message: "Hello from the users route!" });
 }
