4e94ec0000a7c4fe32f72d4d34aa2ce9b598fbb5
haneesh-bandaru <haneeshbandaru05@gmail.com>
2024-11-30 00:17:42 +0530

    fixed the issues

--- app/api/(projects)/fetch-projects/route.ts
+++ app/api/(projects)/fetch-projects/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-//   const projects = await Project.find();
-//   res.json(projects);
+  //   const projects = await Project.find();
+  //   res.json(projects);
+  return NextResponse.json({ message: "Hello from the users route!" });
 }
