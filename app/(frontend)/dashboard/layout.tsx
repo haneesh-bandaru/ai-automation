ccc0db1684a893543a520831307fb0b1a86da11b
haneesh-bandaru <haneeshbandaru05@gmail.com>
2024-11-30 00:32:03 +0530

    fixed the issues

--- app/(frontend)/dashboard/layout.tsx
+++ app/(frontend)/dashboard/layout.tsx
@@ -1,11 +1,22 @@
-import { Sidebar } from "@/components/dashboard/sidebar";
+"use client";
 import { Header } from "@/components/dashboard/header";
+import { Sidebar } from "@/components/dashboard/sidebar";
+import React from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [showHeader, setShowHeader] = React.useState(false);
+
+  React.useEffect(() => {
+    if (typeof window !== 'undefined' && window.location.pathname !== '/dashboard/assign-tasks') {
+      setShowHeader(true);
+    }
+    { typeof window !== 'undefined' && window.location.pathname !== '/dashboard/assign-ttasks' && <Header /> }
+  }, []);
+
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
